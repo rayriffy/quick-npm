4f288000230d4ecdd3a467370b2ddf94876d3c3c
Phumrapee Limpianchop <git@rayriffy.com>
2026-05-22 18:44:31 +0700

    feat: update, tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   dts: true,
   sourcemap: true,
   clean: true,
